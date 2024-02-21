404aca978827eb4a3de51a1b20a08c34ac875d70
NourMawla98 <90239368+NourMawla98@users.noreply.github.com>
2024-02-21 16:01:04 +0200

    initial commit

---
+++ SignalR/Controllers/SignalRController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignalRController : ControllerBase
+    {
+        private readonly IHubContext<TestChatHub> _hubContext;
+
+        public SignalRController(IHubContext<TestChatHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost("send")]
+        public async Task SendMessage(string user, string message)
+        {
+            await _hubContext.Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
